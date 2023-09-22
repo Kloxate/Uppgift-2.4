84528b3a9164ec5f5b8889f01b1b9192a2dda723
Pingvu <nhlpool27@gmail.com>
2023-09-22 15:29:42 +0200

    Add project files.

---
+++ Uppgift 2.4/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace School_work
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Lön för person 1");
+            string Person_1 = Console.ReadLine();
+            Console.WriteLine("Lön för person 2");
+            string Person_2 = Console.ReadLine();
+            Console.WriteLine("Lön för person 3");
+            string Person_3 = Console.ReadLine();
+            int Lön1 = int.Parse(Person_1);
+            int Lön2 = int.Parse(Person_2);
+            int Lön3 = int.Parse(Person_3);
+            int Totalt = Lön1 + Lön2 + Lön3;
+            int Medellön = Totalt / 3;
+            Console.WriteLine(Medellön + "kr är medellönen");
+
+        }
+    }
+}
